5bc8f86b1e7ebaf3eccc39ae1b30b0d8cdfa7457
Eduardo Bonato <ebonato@gmail.com>
2024-03-05 21:53:45 -0300

    Update chocolateyinstall.ps1

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,24 +1,25 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64 = 'https://cdn.mysql.com//Downloads/Connector-ODBC/8.3/mysql-connector-odbc-8.3.0-winx64.msi'
+#$url64 = 'https://cdn.mysql.com/archives/mysql-connector-odbc-8.0/mysql-connector-odbc-8.0.33-winx64.msi'
+$url32 = 'https://cdn.mysql.com/archives/mysql-connector-odbc-8.0/mysql-connector-odbc-8.0.33-win32.msi'
 
 $packageArgs = @{
   packageName = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   fileType = 'msi'
-  url64bit = $url64
+  url = $url32
 
-  softwareName = 'mysql odbc driver *'
+  softwareName = 'mysql odbc win32 driver *'
 
-  checksum64 = 'ffecc1317d39a546ae73f7ac2fa5031b6dc5b6ab41e0eb0af3089f2e05e98fea'
-  checksumType64 = 'sha256'
+  checksum = '6999cb630a82876dda4e595e0e74ffbb'
+  checksumType = 'md5'
 
   silentArgs = "/qn"
   validExitCodes = @(0)
 }
 
-if (-not ([System.Environment]::Is64BitOperatingSystem)) {
-  throw "This package requires a 64-bit version of Windows."
-}
+#if (-not ([System.Environment]::Is64BitOperatingSystem)) {
+#  throw "This package requires a 64-bit version of Windows."
+#}
 
 Install-ChocolateyPackage @packageArgs
